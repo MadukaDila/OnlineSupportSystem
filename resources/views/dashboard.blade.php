c76322c37391b3e3f7282cde2495d82ecd12e0d0
Madukadila9 <madukadilshan9@gmail.com>
2023-07-10 22:12:32 +0530

    showing ticket list and pagination is completed.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,71 @@
+@extends('layout.sidenavbar')
+<style>
+    .page-item.active .page-link {
+        z-index: 3 !important;
+        color: #fff !important;
+        background-color: #003366 !important;
+        border-color: #003366 !important;
+    }
+    
+    .pagination .page-item .page-link:hover {
+        background-color: #94a1ad;
+        border-color: #94a1ad;
+        color: #ffffff;
+    }
+    
+    .page-link {
+        color: #003366 !important;
+    }
+</style>
+@section('content')
+    <h1>Support Tickets</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Customer Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone Number</th>
+                <th scope="col">Reference Number</th>
+                <th scope="col">Description</th>
+                <th scope="col">Status</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+                $count = ($tickets->currentPage() - 1) * $tickets->perPage() + 1;
+            @endphp
+            @foreach ($tickets as $ticket)
+                <tr>
+                    <th scope="row">{{ $count }}</th>
+                    <td>{{ $ticket->customer_name }}</td>
+                    <td>{{ $ticket->email }}</td>
+                    <td>{{ $ticket->phone_number }}</td>
+                    <td>{{ $ticket->reference_number }}</td>
+                    <td>{{ $ticket->problem_description }}</td>
+                    <td>{{ $ticket->status }}</td>
+                    <td>
+                        <button class="btn btn-primary" style="background-color: #003366; " onclick="redirectToNewPage({{ $ticket->id }})">View</button>
+                    </td>
+                </tr>
+                @php
+                    $count++;
+                @endphp
+            @endforeach
+        </tbody>
+    </table>
+
+    <div class="d-flex justify-content-center">
+        {{ $tickets->links('pagination::bootstrap-4') }}
+    </div>
+@endsection
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script>
+    function redirectToNewPage(ticketId) {
+        var url = '/supportreply/' + ticketId;
+        window.location.href = url;
+    }
+</script>
